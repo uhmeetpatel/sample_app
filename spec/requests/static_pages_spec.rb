a169776fd2cc1a2f69d9099714afe8f02691e043
Amit Patel <uhmeet.patel@gmail.com>
2014-03-04 16:26:59 -0600

    add contact page, use let fcn to refactor test, use postgresql for db

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,6 +11,7 @@ require 'spec_helper'
 # end
 
 describe "Static pages" do
+    let(:base_title) { "Ruby on Rails Tutorial Sample App" }
     describe "Home page" do
         it "should have the content 'Sample App'" do
             visit '/static_pages/home'
@@ -18,7 +19,7 @@ describe "Static pages" do
         end
         it "should have the right title" do
             visit '/static_pages/home'
-            expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+            expect(page).to have_title("#{base_title} | Home")
         end
     end
     describe "Help page" do
@@ -28,7 +29,7 @@ describe "Static pages" do
         end
         it "should have the right title" do
             visit '/static_pages/help'
-            expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+            expect(page).to have_title("#{base_title} | Help")
         end
     end
     describe "About page" do
@@ -38,7 +39,17 @@ describe "Static pages" do
         end
         it "should have the right title" do
             visit '/static_pages/about'
-            expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+            expect(page).to have_title("#{base_title} | About")
+        end
+    end
+    describe "Contact page" do
+        it "should have the content 'Contact'" do
+            visit '/static_pages/contact'
+            expect(page).to have_content('Contact')
+        end
+        it "should have the right title" do
+            visit '/static_pages/contact'
+            expect(page).to have_title("#{base_title} | Contact")
         end
     end
 end
