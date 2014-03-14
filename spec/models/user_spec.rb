8d1edc01685d4a6cfc2a42f675bcbbfef47f6d22
Amit Patel <uhmeet.patel@gmail.com>
2014-03-14 17:01:24 -0500

    post chapter 6 exercises

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,6 +36,15 @@ describe User do
         end
     end
 
+    describe "email address with mixed case" do
+        let(:mixed_case_email) { "Foo@ExAmPLE.cOM" }
+        it "should be saved as all lower case" do
+            @user.email = mixed_case_email
+            @user.save
+            expect(@user.reload.email).to eq mixed_case_email.downcase
+        end
+    end
+
     describe "when password is not present" do
         before { @user = User.new(name: "Example User", email: "user@example.com",
                               password: " ", password_confirmation: " ") }
@@ -75,7 +84,7 @@ describe User do
     describe "when email format is invalid" do
         it "should be invalid" do
             addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+                     foo@bar_baz.com foo@bar+baz.com foo@bar..com]
             addresses.each do |invalid_address|
                 @user.email = invalid_address
                 expect(@user).not_to be_valid
@@ -85,7 +94,7 @@ describe User do
         
     describe "when email format is valid" do
         it "should be valid" do
-            addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+            addresses = %w[user@foo.COM help@myorg.co.jp A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
             addresses.each do |valid_address|
                 @user.email = valid_address
                 expect(@user).to be_valid
